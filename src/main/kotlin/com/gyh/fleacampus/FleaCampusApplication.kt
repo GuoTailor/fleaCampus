70e6fc6b9564ace7cea896d27b28b743afbdbf34
GYH <2350871838@qq.com>
2021-04-16 10:51:30 +0800

    first commit

---
+++ src/main/kotlin/com/gyh/fleacampus/FleaCampusApplication.kt
@@ -0,0 +1,11 @@
+package com.gyh.fleacampus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FleaCampusApplication
+
+fun main(args: Array<String>) {
+    runApplication<FleaCampusApplication>(*args)
+}
---
+++ src/test/kotlin/com/gyh/fleacampus/FleaCampusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gyh.fleacampus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FleaCampusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
